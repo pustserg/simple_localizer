f24cf767c6ea5d2308a52360f5d54f2cba2caf1e
Sergey Pustovalov <pustserg@yandex.ru>
2020-05-09 15:10:53 +0300

    Copiable locale

---
+++ test/locale_test.rb
@@ -0,0 +1,45 @@
+require "minitest/autorun"
+
+require_relative '../models/locale.rb'
+
+class LocaleTest < Minitest::Test
+  def test_parse
+    yaml = {
+      "ru" => {
+        "messages" => {
+          "hello" => "Привет"
+        }
+      }
+    }
+    locale = Locale.parse(yaml)
+
+    assert locale.is_a?(Locale)
+    assert_equal locale.language, 'ru'
+    assert_equal locale.data.size, 1
+    assert_equal locale.find_node_by_key('messages.hello').value, 'Привет'
+  end
+
+  def test_copy
+    yaml = {
+      "ru" => {
+        "messages" => {
+          "hello" => "Привет"
+        }
+      }
+    }
+    locale = Locale.parse(yaml)
+    other_locale = locale.copy('de')
+
+    assert other_locale.is_a?(Locale)
+    assert_equal other_locale.language, 'de'
+    assert_equal other_locale.data.size, 1
+
+    node = other_locale.data.first
+    assert_nil node.value
+    assert_equal node.children.size, 1
+
+    first_child = node.children.first
+    assert first_child.children.empty?
+    assert_equal first_child.value, 'Fill me'
+  end
+end
